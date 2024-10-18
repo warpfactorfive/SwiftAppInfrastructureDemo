f1bdc2bac24d88e6b1bd41ac8e7915715f3365bc
warpfactorfive <terry.sd@gmail.com>
2024-10-18 13:55:22 -0700

    Initial Commit

---
+++ SwiftAppInfrastructure/SwiftAppInfrastructureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppInfrastructureApp.swift
+//  SwiftAppInfrastructure
+//
+//  Created by Terry on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppInfrastructureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
